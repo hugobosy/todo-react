d46a2db21eb6c00a5d2a84a1829c3f961c3419bf
HugoBosy <hkliszcz@gmail.com>
2022-05-27 09:30:16 +0200

    add structure form AddTask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,12 +2,21 @@ import React, {useState} from "react";
 
 export const AddTask = () => {
 
-    const [state, setState] = useState('')
+    const [state, setState] = useState({
+        text: '',
+        check: false,
+        date: "2022-05-27"
+    })
 
     return (
         <div>
-            <p>Dodaj task</p>
-            <hr/>
+            <input type="text" placeholder="dodaj zadanie" value={state.text}/>
+            <input type="checkbox" checked={state.check}/>
+            <label>Priorytet</label>
+            <br/>
+            <label>Do kiedy zrobić</label>
+            <input type="date" value={state.date} min="2022-05-27" max="2022-12-31"/>
+            <button>Dodaj</button>
         </div>
     )
 }
